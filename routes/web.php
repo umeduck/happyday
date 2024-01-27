6fb0dd2f9ab67f260c19e65b8151c123a41c682e
umeduck <ume_duck@outlook.jp>
2024-01-28 00:24:34 +0900

    inspection

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,10 @@ Route::get('/', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/welcome', function() {
+  return view('welcome');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
