7ed9a05e17576a3272d610594c6b0fc59fc4cdb7
umeduck <ume_duck@outlook.jp>
2024-02-04 16:26:38 +0900

    fix register.vue

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,5 +1,5 @@
 <script setup>
-import GuestLayout from '@/Layouts/GuestLayout.vue';
+import LoginLayout from '@/Layouts/LoginLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
@@ -23,7 +23,7 @@ const submit = () => {
 </script>
 
 <template>
-  <GuestLayout>
+  <LoginLayout>
     <Head title="Register" />
 
     <form @submit.prevent="submit">
@@ -116,5 +116,5 @@ const submit = () => {
         </PrimaryButton>
       </div>
     </form>
-  </GuestLayout>
+  </LoginLayout>
 </template>
