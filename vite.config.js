e64447b3ad523c76d0af327d0163162eb5a8e4d6
umeduck <ume_duck@outlook.jp>
2024-02-22 21:23:58 +0900

    inspection

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,21 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
+  server: {
+    https: true,
+  },
+  plugins: [
+    laravel({
+      input: 'resources/js/app.js',
+      refresh: true,
+    }),
+    vue({
+      template: {
+          transformAssetUrls: {
+              base: null,
+              includeAbsolute: false,
+          },
+        },
+    }),
+  ],
 });
